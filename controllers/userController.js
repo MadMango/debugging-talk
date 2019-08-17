fec823e526fafa092c1bb92594f1fd9bd9fdfbbd
madmango <msf11235813@gmail.com>
2019-08-18 00:24:15 +0100

    initial setup

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const faker = require('faker');
+
+const generateRandomUser = () => ({
+  name: faker.name.firstName(),
+  department: faker.commerce.department(),
+  email: faker.internet.email()
+})
+
+const users = {
+  1: generateRandomUser(),
+  2: generateRandomUser(),
+  3: generateRandomUser()
+}
+
+const getAllUsers = () => users
+const getUserName = userNameID => users[userNameID].name
+
+module.exports = { getAllUsers, getUserName }
