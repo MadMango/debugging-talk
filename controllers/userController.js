a5930252339c0a18efb8ade0823e0bb5018ec5b6
madmango <verymadmango@gmail.com>
2022-02-03 15:13:24 +0000

    formatting

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,18 @@
-const faker = require('faker');
+const faker = require("faker");
 
 const generateRandomUser = () => ({
   name: faker.name.firstName(),
   department: faker.commerce.department(),
-  email: faker.internet.email()
-})
+  email: faker.internet.email(),
+});
 
 const users = {
   1: generateRandomUser(),
   2: generateRandomUser(),
-  3: generateRandomUser()
-}
+  3: generateRandomUser(),
+};
 
-const getAllUsers = () => users
-const getUserName = userNameID => users[userNameID].name
+const getAllUsers = () => users;
+const getUserName = (userNameID) => users[userNameID].name;
 
-module.exports = { getAllUsers, getUserName }
+module.exports = { getAllUsers, getUserName };
